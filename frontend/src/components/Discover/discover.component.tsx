9e76213765b89e2a6882b1f08e41bf1819f3b756
shinelight999 <sydneycardy@gmail.com>
2022-11-15 13:52:54 -0500

    Added titles to Pages

--- frontend/src/components/Discover/discover.component.tsx
+++ frontend/src/components/Discover/discover.component.tsx
@@ -1,4 +1,7 @@
+import { useTitle } from "../../hooks/useTitle.hook";
+
 const Discover = () => {
+    useTitle("Discover");
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <h1>Discover Dummy Text</h1>
--- frontend/src/components/Favorited/favorited.component.tsx
+++ frontend/src/components/Favorited/favorited.component.tsx
@@ -1,4 +1,7 @@
+import { useTitle } from "../../hooks/useTitle.hook";
+
 const Favorited = () => {
+    useTitle("Favorited");
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <h1>Favorited Dummy Text</h1>
