fb80b77dca736074376461fb11cfbdad229afb83
chaaf <chaaf@stevens.edu>
2022-12-02 16:42:04 -0500

    backend

---
+++ backend/src/data/interfaces.ts
@@ -0,0 +1,3 @@
+export interface todo {
+    fillIn: String
+}
---
+++ backend/src/routes/users.route.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response, Router } from "express";
+
+export const usersRouter: Router = express.Router();
+
+usersRouter.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+
+
+/* 
+
+/auth/register -> createa user:
+
+{
+	_id: "507f1f77bcf86cd799439011",
+	username: “mikeydellWooHoo”,
+	password: ################, ← hashed value
+	firstName: “Mike”,
+	lastName: “Dell”,
+	profileImage:  
+	contactInfo: {
+				phoneNumber: “201-204-2482”,
+				email: “blahblah@gmail.com”,
+				personalWebsite: “hoboken.com”
+				currentRole: “Senior CS Major”
+            }, 
+	socialMedia: [
+        {
+            _id: “12497c4c-b923-422f-98bb-801f8bd737ba”, 
+            profileURL: “https://twitter.com/CityofHoboken”
+        }
+    ],
+    likes: [
+        {
+            _id: “7e13b59d-18b0-439f-ac68-2d6bfed7ee72”, 
+            like: “Swimming”
+        },
+        {
+            _id: “7e13b59d-18b0-439f-ac68-2d6bfed7ee71”, 
+            like: Running
+        }, 
+    ],
+    dislikes: [
+        {
+            _id: "5349b4ddd2781d08c09890f3",
+            dislike: “Running”
+        }, 
+    ],
+    favortiedUsers: [“507f1f77bcf86cd799439011”] 
+}
+
+
+*/
+
+
+
+
+
+
+
+
+});
