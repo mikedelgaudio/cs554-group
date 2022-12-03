f007be4bac22db6e7ccc65ec926431ea7ff68116
Mike DelGaudio <delgaudiomike@gmail.com>
2022-12-03 16:37:26 -0500

    adding user model and redux changes

--- frontend/src/redux/app/app.reducer.ts
+++ frontend/src/redux/app/app.reducer.ts
@@ -1,93 +1,107 @@
-import { v4 as uuidv4 } from "uuid";
-import * as types from "./app.types";
-
 const INITIAL_STATE = {
-  activeTeam: {
-    id: "team-1",
-    trainerName: "Mike DelGaudio",
-  },
-  teams: [
-    {
-      id: "team-1",
-      trainerName: "Mike DelGaudio",
-      caughtPokemon: [
-        {
-          id: "1",
-          name: "bulbasaur",
-          imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-          imgAlt: "bulbasaur profile",
-        },
-      ],
+  user: {
+    id: "default-user-1",
+    username: "graffixnyc",
+    firstName: "Patrick",
+    lastName: "Hill",
+    profileImage: "https://via.placeholder.com/150",
+    contactInfo: {
+      phoneNumber: "718-555-5555",
+      email: "phill@stevens.edu",
+      personalWebsite: "http://www.patrickhill.nyc/",
+      currentRole: "test",
     },
-  ],
+    socialMedia: [
+      {
+        id: "1",
+        profileURL: "test",
+      },
+    ],
+    likes: [
+      {
+        id: "1",
+        name: "test",
+      },
+    ],
+    dislikes: [
+      {
+        id: "1",
+        name: "test",
+      },
+    ],
+    favoritedUsers: [
+      {
+        id: "1",
+      },
+    ],
+  },
 };
 
 const appReducer = (state = INITIAL_STATE, action: any) => {
   switch (action.type) {
-    case types.ADD_POKEMON_TO_TEAM:
-      return {
-        ...state,
-        teams: state.teams.map(team => {
-          if (team.id === action.payload.teamId) {
-            return {
-              ...team,
-              caughtPokemon: [
-                ...team.caughtPokemon,
-                {
-                  id: action.payload.pokemonId,
-                  name: action.payload.pokemonName,
-                  imgSrc: action.payload.imgSrc,
-                  imgAlt: action.payload.imgAlt,
-                },
-              ],
-            };
-          } else return team;
-        }),
-      };
-    case types.CREATE_TEAM:
-      return {
-        ...state,
-        teams: [
-          {
-            id: uuidv4(),
-            trainerName: action.payload.trainerName,
-            caughtPokemon: [],
-          },
-          ...state.teams,
-        ],
-      };
-    case types.REMOVE_POKEMON_FROM_TEAM:
-      return {
-        ...state,
-        teams: state.teams.map(team => {
-          if (team.id === action.payload.teamId) {
-            return {
-              ...team,
-              caughtPokemon: team.caughtPokemon.filter(
-                pokemon => pokemon.id !== action.payload.pokemonId,
-              ),
-            };
-          } else return team;
-        }),
-      };
-    case types.SET_SELECTED_TEAM:
-      return {
-        ...state,
-        activeTeam: {
-          ...state.activeTeam,
-          id: action.payload.teamId,
-          trainerName: action.payload.trainerName,
-        },
-      };
-    case types.DELETE_TEAM:
-      // ? Throw error?
-      if (state.activeTeam.id === action.payload.teamId) return;
+    // case types.ADD_POKEMON_TO_TEAM:
+    //   return {
+    //     ...state,
+    //     teams: state.teams.map(team => {
+    //       if (team.id === action.payload.teamId) {
+    //         return {
+    //           ...team,
+    //           caughtPokemon: [
+    //             ...team.caughtPokemon,
+    //             {
+    //               id: action.payload.pokemonId,
+    //               name: action.payload.pokemonName,
+    //               imgSrc: action.payload.imgSrc,
+    //               imgAlt: action.payload.imgAlt,
+    //             },
+    //           ],
+    //         };
+    //       } else return team;
+    //     }),
+    //   };
+    // case types.CREATE_TEAM:
+    //   return {
+    //     ...state,
+    //     teams: [
+    //       {
+    //         id: uuidv4(),
+    //         trainerName: action.payload.trainerName,
+    //         caughtPokemon: [],
+    //       },
+    //       ...state.teams,
+    //     ],
+    //   };
+    // case types.REMOVE_POKEMON_FROM_TEAM:
+    //   return {
+    //     ...state,
+    //     teams: state.teams.map(team => {
+    //       if (team.id === action.payload.teamId) {
+    //         return {
+    //           ...team,
+    //           caughtPokemon: team.caughtPokemon.filter(
+    //             pokemon => pokemon.id !== action.payload.pokemonId,
+    //           ),
+    //         };
+    //       } else return team;
+    //     }),
+    //   };
+    // case types.SET_SELECTED_TEAM:
+    //   return {
+    //     ...state,
+    //     activeTeam: {
+    //       ...state.activeTeam,
+    //       id: action.payload.teamId,
+    //       trainerName: action.payload.trainerName,
+    //     },
+    //   };
+    // case types.DELETE_TEAM:
+    //   // ? Throw error?
+    //   if (state.activeTeam.id === action.payload.teamId) return;
 
-      return {
-        ...state,
-        teams: state.teams.filter(team => team.id !== action.payload.teamId),
-      };
+    //   return {
+    //     ...state,
+    //     teams: state.teams.filter(team => team.id !== action.payload.teamId),
+    //   };
     default:
       return state;
   }
