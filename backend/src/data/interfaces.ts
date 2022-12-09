76efeef5f43e86261b21501fd6cfcd5e5ddbe093
chaaf3 <90810386+chaaf3@users.noreply.github.com>
2022-12-09 16:11:49 -0500

    Update interfaces.ts

--- backend/src/data/interfaces.ts
+++ backend/src/data/interfaces.ts
@@ -1,3 +1,40 @@
-export interface todo {
-    fillIn: String
+interface contactInfo {
+    phoneNumber: string;
+    email: string;
+    personalWebsite: string;
+    currentRole: string;
 }
+
+
+interface socialMedia {
+    _id: string;
+    profileUrl: string;
+}
+interface like {
+    _id: string;
+    like: string;
+}
+interface dislike {
+    _id: string;
+    dislike: string;
+}
+
+
+interface user {
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    profileImage: string;
+    contactInfo: contactInfoI;
+    socialMedias: Array<socialMediaI>;
+    likes: Array<likeI>;
+    dislikes: Array<dislikeI>;
+    favoritedUsers: Array<string>;
+};
+
+interface users {
+    users: Array<userI>;
+}
+
+
