088897c70d55de96c65a436c3a0701cf132c2ee8
shinelight999 <sydneycardy@gmail.com>
2022-12-16 12:12:34 -0500

    Edited helper functions

--- frontend/src/components/Profile/helper.tsx
+++ frontend/src/components/Profile/helper.tsx
@@ -1,193 +1,160 @@
 import axios from "axios";
 
-// Function to change username in api
+// Change username
 export const changeUsername = async (id: any, newUserName: string) => { 
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change username
     user.data.username = newUserName;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${id}`, user.data);
 };
 
-// Function to change First Name in API
+// Change First Name
 export const changeFirstName = async (id: any, newFirstName: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change first name
     user.data.firstName = newFirstName;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${id}`, user.data);
 };
 
-// Function to change Last Name in API
+// Change Last Name
 export const changeLastName = async (id: any, newLastName: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change last name
     user.data.lastName = newLastName;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${id}`, user.data);
 };
 
-// Function to change Profile Picture in API
+// Change Profile Picture
 export const changeProfilePicture = async (id: any, newProfilePicture: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change profile picture
     user.data.profileImage = newProfilePicture;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    if (user.data.profileImage) {
+        await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    } else {
+        await axios.post(`http://localhost:3000/users/${id}`, user.data);
+    }
 }
 
-// Function to change Phone Number in API
+// Change Phone Number
 export const changePhoneNumber = async (id: any, newPhoneNumber: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change phone number
     user.data.contactInfo.phoneNumber = newPhoneNumber;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    if (user.data.contactInfo.phoneNumber) {
+        await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    } else {
+        await axios.post(`http://localhost:3000/users/${id}`, user.data);
+    }
 }
 
-// Function to change Email in API
+// Change Email
 export const changeEmail = async (id: any, newEmail: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change email
     user.data.contactInfo.email = newEmail;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    if (user.data.contactInfo.email) {
+        await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    } else {
+        await axios.post(`http://localhost:3000/users/${id}`, user.data);
+    }
 }
 
-// Function to change Website in API
+// Change Website
 export const changeWebsite = async (id: any, newWebsite: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change website
     user.data.contactInfo.website = newWebsite;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    if (user.data.contactInfo.website) {
+        await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    } else {
+        await axios.post(`http://localhost:3000/users/${id}`, user.data);
+    }
 }
 
-// Function to change Occupation in API
+// Change Occupation
 export const changeOccupation = async (id: any, newOccupation: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // change occupation
     user.data.occupation = newOccupation;
-    // update user in api
     // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    if (user.data.occupation) {
+        await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    } else {
+        await axios.post(`http://localhost:3000/users/${id}`, user.data);
+    }
 }
 
-// Function to add another social media object
+// Add Social Media
 export const addSocialMedia = async (id: any, socialMediaURL: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // add social media object
     user.data.socialMedia.push({profileURL: socialMediaURL});
-    // update user in api
     // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    if (user.data.socialMedia) {
+        await axios.put(`http://localhost:3000/users/${id}`, user.data);
+    } else {
+        await axios.post(`http://localhost:3000/users/${id}`, user.data);
+    }
 }
 
-// Function to delete a social media object
+// Delete Social Media
 export const deleteSocialMedia = async (profileID: any, socialMediaID: any) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${profileID}`);
-    // delete social media object
     user.data.socialMedia.splice(socialMediaID, 1);
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${profileID}`, user.data);
 }
 
-// Function to add a Like Object
+// Add a Like
 export const addLike = async (id: any, like: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // add like object
     user.data.likes.push({name: like});
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${id}`, user.data);
 }
 
-// Function to delete a Like Object
+// Delete a Like
 export const deleteLike = async (profileID: any, likeID: any) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
-    const user = await axios.get(`http://localhost:3000/users/${profileID}`);
-    // delete like object
-    user.data.likes.splice(likeID, 1);
-    // update user in api
-    // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${profileID}`, user.data);
+    await axios.delete(`http://localhost:3000/users/likes/${likeID}`)
 }
 
-// Function to add a Dislike Object
+// Add a Dislike
 export const addDislike = async (id: any, dislike: string) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // add dislike object
     user.data.dislikes.push({name: dislike});
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${id}`, user.data);
 }
 
-// Function to delete a Dislike Object
+// Delete a Dislike
 export const deleteDislike = async (profileID: any, dislikeID: any) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
-    const user = await axios.get(`http://localhost:3000/users/${profileID}`);
-    // delete dislike object
-    user.data.dislikes.splice(dislikeID, 1);
-    // update user in api
-    // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${profileID}`, user.data);
+    await axios.delete(`http://localhost:3000/users/dislikes/${dislikeID}`)
 }
 
-// Function to add a Favorited User
+// Add a Favorited User
 export const addFavoritedUser = async (id: any, favoritedUserID: any) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
     const user = await axios.get(`http://localhost:3000/users/${id}`);
-    // add favorited user
     user.data.favoritedUsers.push({id: favoritedUserID});
-    // update user in api
     // Change url to 3001 when using backend -Sydney
     await axios.put(`http://localhost:3000/users/${id}`, user.data);
 }
 
-// Function to delete a favorited user
+// Delete a Favorited User
 export const deleteFavoritedUser = async (profileID: any, favoritedUserID: any) => {
-    // find user in api by id
     // Change url to 3001 when using backend -Sydney
-    const user = await axios.get(`http://localhost:3000/users/${profileID}`);
-    // delete favorited user
-    user.data.favoritedUsers.splice(favoritedUserID, 1);
-    // update user in api
-    // Change url to 3001 when using backend -Sydney
-    await axios.put(`http://localhost:3000/users/${profileID}`, user.data);
+    await axios.delete(`http://localhost:3000/users/favorited/${favoritedUserID}`)
 }
