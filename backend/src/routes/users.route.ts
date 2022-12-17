2b5483e8838128a9722001ac75b4a82a1f0af283
Justin Wing <justinwing@hotmail.com>
2022-12-17 16:49:22 -0500

    update validations

--- backend/src/routes/users.route.ts
+++ backend/src/routes/users.route.ts
@@ -68,32 +68,38 @@ usersRouter.post('/:id/editUser', async (req: Request, res: Response) => {
 
         //console.log(username, email, password);
 
-        let userBody = {};
-        if(req.body.username){
-        if (typeof req.body.username != "string" || !req.body.username) {
-            res.status(400).json({error:"Updated username must be a valid string"});
-        }else{
-            userBody.username = req.body.username;
-        }
-    }
+        let userObj = {} as user;
+
     if(req.body.username){
         if (typeof req.body.username != "string" || !req.body.username) {
-            res.status(400).json({error:"Updated username must be a valid string"});
+          return  res.status(400).json({error:"Updated username must be a valid string"});
+        }
+        else{
+            userObj.username = req.body.username;
         }
     }
     if(req.body.firstName){
         if (typeof req.body.firstName != "string" || !req.body.firstName) {
-            res.status(400).json({error:"Updated first name must be a valid string"});
+           return res.status(400).json({error:"Updated first name must be a valid string"});
+        }
+        else{
+            userObj.firstName = req.body.firstName;
         }
     }
     if(req.body.lastName){
         if (typeof req.body.lastName != "string" || !req.body.lastName) {
-            res.status(400).json({error:"Updated last name must be a valid string"});
+           return res.status(400).json({error:"Updated last name must be a valid string"});
+        }
+        else{
+            userObj.lastName = req.body.lastName;
         }
     }
     if(req.body.profileImage){
         if (typeof req.body.profileImage != "string" || !req.body.profileImage) {
-            res.status(400).json({error:"Updated profile image must be a valid string"});
+            return res.status(400).json({error:"Updated profile image must be a valid string"});
+        }
+        else{
+            userObj.profileImage = req.body.profileImage;
         }
     }
     if(req.body.contactInfo){
@@ -101,35 +107,45 @@ usersRouter.post('/:id/editUser', async (req: Request, res: Response) => {
        
         for (const [k, v] of Object.entries(req.body.contactInfo)) {
             if (!contactFields.includes(k)){
-                res.status(400).json({error: "Contact Field must be either phone number, email, personal website, or current role"})
+               return res.status(400).json({error: "Contact Field must be either phone number, email, personal website, or current role"})
             }
             if(typeof(v) != "string"){
-                res.status(400).json({error: "Contact Info must be string"})
+              return res.status(400).json({error: "Contact Info must be string"})
             }
     }
+        userObj.contactInfo = req.body.contactInfo;
+
+    
 }
     if(req.body.socialMedias){
         if (Array.isArray(req.body.socialMedias)) {
-            res.status(400).json({error:"Updated social medias must be a valid array"});
+           return res.status(400).json({error:"Updated social medias must be a valid array"});
+        }
+        else{
+            userObj.socialMedias = req.body.socialMedias;
         }
     }
     if(req.body.likes){
         if (Array.isArray(req.body.likes) || !req.body.likes) {
-            res.status(400).json({error:"Updated social medias must be a valid string"});
+           return res.status(400).json({error:"Updated likes must be a valid array"});
+        }
+        else{
+            userObj.likes = req.body.likes;
         }
     }
     if(req.body.dislikes){
         if (Array.isArray(req.body.dislikes) || !req.body.dislikes) {
-            res.status(400).json({error:"Updated social medias must be a valid string"});
+           return res.status(400).json({error:"Updated dislikes must be a valid array"});
+        }
+        else{
+            userObj.dislikes = req.body.dislikes;
         }
     }
-
-    
-        let answer = await data.patchUser(userBody, id);
-        res.json(answer);
+        let answer = await data.patchUser(userObj, id);
+        return res.json(answer);
       }
       catch (e) {
-        res.status(404).json({error:e});
+        return res.status(404).json({error:e});
       }
     });
 
